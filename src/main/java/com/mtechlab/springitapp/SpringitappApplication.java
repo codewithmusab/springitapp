d2c3070f3499063efe04ba188e3612e46397c45d
codewithmusab <codewithmusa@gmail.com>
2019-08-28 14:40:51 +0000

    Initials commits

---
+++ src/main/java/com/mtechlab/springitapp/SpringitappApplication.java
@@ -0,0 +1,13 @@
+package com.mtechlab.springitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringitappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringitappApplication.class, args);
+    }
+
+}
